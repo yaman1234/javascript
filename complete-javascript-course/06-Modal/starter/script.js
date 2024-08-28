f10f79c816b106d7ccb9f82f0e8721908f197886
Yaman Maharjan <yamanmaharjan00@gmail.com>
2024-08-28 13:14:49 +0545

    assignment 2 completed

--- complete-javascript-course/06-Modal/starter/script.js
+++ complete-javascript-course/06-Modal/starter/script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const btnsShowModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+function closeModal() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+function showModal() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+
+for (let i = 0; i < btnsShowModal.length; i++) {
+  btnsShowModal[i].addEventListener('click', showModal);
+}
+
+document.addEventListener('keydown', function (e) {
+  console.log(e);
+  if (e.keyCode === 27) {
+    closeModal();
+  }
+});
+
+// close modal when click outside the modal
+overlay.addEventListener('click', closeModal);
